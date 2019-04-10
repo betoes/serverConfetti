e9c2ee7845a55560a91cfb17053993b246564472
betoes <ragest031@gmail.com>
2019-04-09 19:44:32 -0500

    Se añadieron codigos de estatus, y se añadio busqueda por correo

--- src/rutas/usuario.php
+++ src/rutas/usuario.php
@@ -10,53 +10,124 @@ $app->get('/api/usuario', function (Request $request, Response $response) {
         $resultado = $db->query($sql);
         if ($resultado->rowCount() > 0) {
             $usuarios = $resultado->fetchAll(PDO::FETCH_OBJ);
-            echo json_encode($usuarios);
+            return $response->withJson($usuarios, 200);
         } else {
-            echo json_encode('No existen usuarios en la BBDD');
+            return $response->withJson("No se encontro la info solicitada", 404);
         }
         $resultado = null;
         $db = null;
     } catch (PDOException $th) {
-        echo '{"error": {"text":' . $th->getMessage() . '}';
+        return $response->withJson($th.getMessage(), 500);
     }
 });
+
+$app->get('/api/usuario/{parametro}', function (Request $request, Response $response, array $args) {
+
+    $tipoBusqueda->get_Params('tipo');
+
+    switch ($tipoBusqueda) {
+        //Busqueda por nombre de usuario
+        case 1:
+            $usuario = $args['parametro'];
+            $sql = "SELECT * FROM usuario WHERE usuario = :usuario";
+            try {
+                $db = new db();
+                $db = $db->conDB();
+                $resultado = $db->prepare($sql);
+                $resultado->bindParam(':usuario', $usuario);
+                $resultado->execute();
+                if ($resultado->rowCount() > 0) {
+                    $usuarioEncontrado = $resultado->fetchAll(PDO::FETCH_OBJ);
+                    return $response->withJson($usuarioEncontrado, 200);
+                } else {
+                    echo json_encode('No se encontró el usuario solicitado');
+                    return $response->withJson('No se encontró el usuario solicitado', 404);
+                }
+                $resultado = null;
+                $db = null;
+            } catch (PDOException $th) {
+                return $response->withJson($th.getMessage(), 500);
+            }
+            break;
+        //Busqueda por
+        case 2:
+            $correo = $args['parametro'];
+            $sql = "SELECT * FROM usuario WHERE correo = :correo";
+            try {
+                $db = new db();
+                $db = $db->conDB();
+                $resultado = $db->prepare($sql);
+                $resultado->bindParam(':correo', $correo);
+                $resultado->execute();
+                if ($resultado->rowCount() > 0) {
+                    $usuarioEncontrado = $resultado->fetchAll(PDO::FETCH_OBJ);
+                    return $response->withJson($usuarioEncontrado, 200);
+                } else {
+                    return $response->withJson('No se encontró el usuario solicitado', 404);
+                }
+                $resultado = null;
+                $db = null;
+            } catch (PDOException $th) {
+                return $response->withJson($th.getMessage(), 500);
+            }
+            break;
+    }
+    
+});
+
+
 
 $app->post('/api/usuario/nuevo', function (Request $request, Response $response) {
     $nombre = $request->getParam('nombre');
     $apellido = $request->getParam('apellido');
     $password = $request->getParam('password');
+    $correo = $request->getParam('correo');
     $usuario = $request->getParam('usuario');
     $telefono = $request->getParam('telefono');
     $isAdmin = $request->getParam('isAdmin');
 
-    $sqlSelect = "SELECT * FROM usuario WHERE usuario = :usuario";
+    $sqlSelectUsuario = "SELECT * FROM usuario WHERE usuario = :usuario";
+    $sqlSelectCorreo = "SELECT * FROM usuario WHERE correo = :correo";
 
-    $sqlInsert = "INSERT INTO usuario (nombre, apellido, password, usuario, telefono, isAdmin) VALUES
-                    (:nombre, :apellido, :password, :usuario, :telefono, :isAdmin)";
+    $sqlInsert = "INSERT INTO usuario (nombre, apellido, password, correo, usuario, telefono, isAdmin) VALUES
+                    (:nombre, :apellido, :password, :correo, :usuario, :telefono, :isAdmin)";
     try {
         $db = new db();
         $db = $db->conDB();
-        $resultado = $db->prepare($sqlSelect);
-        $resultado->bindParam(':usuario', $usuario);
-        $resultado->execute();
-
-        if ($resultado->rowCount() > 0) {
-            echo json_encode("El usuario ya existe");
+        $resultadoUsuario = $db->prepare($sqlSelectUsuario);
+        $resultadoUsuario->bindParam(':usuario', $usuario);
+        $resultadoUsuario->execute();
+        
+        //Se revisa si el usuario ya está registrado en la BD
+        if ($resultadoUsuario->rowCount() > 0) {
+            return $response->withJson("El usuario ya está registrado", 400);
         } else {
-            $resultado = $db->prepare($sqlInsert);
-            $resultado->bindParam(':nombre', $nombre);
-            $resultado->bindParam(':apellido', $apellido);
-            $resultado->bindParam(':password', $password);
-            $resultado->bindParam(':usuario', $usuario);
-            $resultado->bindParam(':telefono', $telefono);
-            $resultado->bindParam(':isAdmin', $isAdmin);
-            $resultado->execute();
-            echo json_encode('Usuario añadido');
-        }
+            $resultadoCorreo = $db->prepare($sqlSelectCorreo);
+            $resultadoCorreo->bindParam(':correo', $correo);
+            $resultadoCorreo->execute();
 
-        $resultado = null;
+            //Se revisa que el correo no este en uso
+            if($resultadoCorreo->rowCount() > 0) {
+                return $response->withJson("El correo ya está registrado", 400);
+            } else {
+
+                //Se termina de preparar la consulta y se ejecuta
+                $resultadoInsert = $db->prepare($sqlInsert);
+                $resultadoInsert->bindParam(':nombre', $nombre);
+                $resultadoInsert->bindParam(':apellido', $apellido);
+                $resultadoInsert->bindParam(':password', $password);
+                $resultadoInsert->bindParam(':correo', $correo);
+                $resultadoInsert->bindParam(':usuario', $usuario);
+                $resultadoInsert->bindParam(':telefono', $telefono);
+                $resultadoInsert->bindParam(':isAdmin', $isAdmin);
+                $resultadoInsert->execute();
+                return $response->withJson("Se ha registrado con exito", 201);
+            }
+        }
+        $resultadoUsuario = null;
+        $resultadoInsert = null;
         $db = null;
     } catch (PDOException $th) {
-        echo '{"error": {"text":' . $th->getMessage() . '}';
+        return $response->withJson($th.getMessage(), 500);
     }
 });
